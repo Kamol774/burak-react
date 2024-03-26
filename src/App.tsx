f07a0686ad7681d6b824f68ebbbc980bf703f5d5
Kamol774 <chust1222@gmail.com>
2024-03-26 14:56:31 +0500

    fix: organize header and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
 import React from 'react';
 import './css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, useLocation } from 'react-router-dom';
 import { HomePage } from './app/screens/homePage';
 import { ProductsPage } from './app/screens/productsPage';
 import { UserPage } from './app/screens/userPage';
 import { OrdersPage } from './app/screens/ordersPage';
+import { HomeNavbar } from './app/components/headers/HomeNavbar';
+import { OtherNavbar } from './app/components/headers/OtherNavbar';
+import { Footer } from './app/components/footer';
 
 function App() {
+  const location = useLocation();
+  console.log("location", location)
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/products">ProductsPage</Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
 
-      {/* A <Switch> looks through its children <Route>s and
+      <Switch> {/* A <Switch> looks through its children <Route>s and
       renders the first one that matches the current URL. */}
-      <Switch>
         <Route path="/products">
           <ProductsPage />
         </Route>
@@ -43,7 +32,8 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
-    </div>
+      <Footer />
+    </>
   )
 }
 
