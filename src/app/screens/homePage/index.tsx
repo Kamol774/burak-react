e2bcc1b028e36b931c6fc67921931717557a0c69
Kamol774 <chust1222@gmail.com>
2024-04-20 19:41:01 +0500

    feat: create homePage Redux configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import Advertisement from "./Advertisement";
@@ -8,6 +8,14 @@ import NewDishes from "./NewDishes";
 import "../../../css/home.css"
 
 export default function HomePage() {
+  // Selector: Store dan => Data ni qabul qilib olish
+  useEffect(() => {
+    // Backend server data request => Data
+
+    // Slice: Data ni => Store ga joylaydi
+  }, []);
+
+
   return <div className={"homepage"}>
     <Statistics />
     <PopularDishes />
